480cf4289889f93f528b01bb2431c8e47498441d
Gilbert KINHOEGBE <projetgilbert4@gmail.com>
2025-09-10 18:21:56 +0100

    Pied de page premier

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+</head>
+<body>
+    <main>
+<footer class="bg-dark text-white py-4 mt-5">
+      <div class="container-fluid bg-dark text-white ">
+        <div class="row p-lg-5 m-lg-4">
+            <div class=" col-lg-4  mt-2 d-flex flex-column gap-4">
+                <h3 class="text-info fw-bold"><i class="fas fa-book me-2 "></i>BiblioBN</h3>
+                <p class="fw-bold text-white">La Bibliothèque Nationale du Bénin <br>offre un accès à une vaste collection <br>d'ouvrages et de ressources documentaires</p>
+                <div class="social-icons d-flex flex-wrap gap-3">
+                   <a href="" class=" text-white text-decoration-none fw-bold"><i class="fab fa-facebook-f me-2 rounded-circle bg-primary  d-inline-flex align-items-center justify-content-center text-white opacity-75" style="width:45px ; height:45px"></i></a>
+                   <a href="" class=" text-white text-decoration-none fw-bold"><i class="fab fa-twitter me-2  rounded-circle bg-primary  d-inline-flex align-items-center justify-content-center text-white opacity-75" style="width:45px ; height:45px""></i></a>
+                   <a href="" class=" text-white text-decoration-none fw-bold"><i class="fab fa-instagram me-2  rounded-circle bg-primary  d-inline-flex align-items-center justify-content-center text-white opacity-75" style="width:45px ; height:45px""></i></a>
+                   <a href="" class=" text-white text-decoration-none fw-bold"><i class="fab fa-youtube me-2  rounded-circle bg-primary  d-inline-flex align-items-center justify-content-center text-white opacity-75" style="width:45px ; height:45px""></i></a>
+                </div>
+            </div>
+            <div class=" col-lg-4  mt-lg-2 mb-3 d-flex flex-column gap-4">
+                <h3 class="text-info fw-bold"><i class="fas fa-link me-2"></i>Liens Rapides</h3>
+              <ul class="list-unstyled d-flex flex-column gap-4">
+                <li class="me-3"><a href="" class="text-white text-decoration-none fw-bold"><i class="fas fa-chevron-right me-2"></i>Accueil</a></li>
+                <li class="me-3 "><a href="" class="text-white text-decoration-none fw-bold" ><i class="fas fa-chevron-right me-2"></i>A propos</a></li>
+                <li class="me-3 "><a href="" class="text-white text-decoration-none fw-bold" ><i class="fas fa-chevron-right me-2"></i>Catalogue</a></li>
+                <li class="me-3 "><a href="" class="text-white text-decoration-none fw-bold"><i class="fas fa-chevron-right me-2"></i>Contact</a></li>
+              </ul>  
+            </div>
+            <div class=" col-lg-4  mt-lg-2 d-flex flex-column gap-4 ">
+                <h3 class="text-info fw-bold"><i class="fas fa-info-circle me-2"></i>Informations</h3>
+              <ul class="list-unstyled d-flex flex-column gap-4">
+                <li><a href="" class="text-white text-decoration-none fw-bold"><i class="fas fa-chevron-right me-2"></i>A Propos</a></li>
+                <li><a href="" class="text-white text-decoration-none fw-bold"><i class="fas fa-chevron-right me-2"></i>Politique de confidentialité</a></li>
+                <li><a href="" class="text-white text-decoration-none fw-bold"><i class="fas fa-chevron-right me-2"></i>Conditions d'utilisation</a></li>
+                <li><a href="" class="text-white text-decoration-none fw-bold"><i class="fas fa-chevron-right me-2"></i>Contact</a></li>
+               </ul>
+            </div>
+            <div class=" col-lg-4 mt-lg-2 mb-5 d-flex flex-wrap gap-1">
+                <h3 class="text-info fw-bold text-decoration-none"><i class="fas fa-map-marker-alt me-2"></i>Nous trouver</h3>
+                <p class="text-white fw-bold">Bibliothèque Nationale du Bénin <br> Bénin ; Abomey Calavi <br>
+                <a href="" class="text-white fw-bold text-decoration-none"><i class="fas fa-phone me-2"></i>+0122225588</a><br>
+                <a href="" class="text-white fw-bold text-decoration-none"><i class="fas fa-envelope me-2"></i>contact@BNB.com</a></p>
+            </div>
+        </div>
+      </div>
+        <hr class ="border-light mt-4">
+        <p class="mb-0 text-center">&copy; 2023 Bibliothèque Nationale du Bénin. Tous droits réservés.</p>
+</footer>
+    </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
