81f64c3aff453bb5a14707be5f958ca7484e8f28
Chédracques ETEKA <etekatobi58@gmail.com>
2025-09-10 18:38:13 +0100

    Page de header premier

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>header</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/14273d579a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header class="py-1 pb-5 align-items-center">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container d-flex justify-content-between align-items-center bg-dark">
+                <a class="navbar-brand text-uppercase fw-bold" href="#">
+                    <span class="bg-primary bg-gradient p-1 rounded-1 text-light"><i class="fas fa-book me-2"></i>BiblioBN</span>
+                </a>    
+                <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>    
+                <div class="collapse navbar-collapse justify-content-end bg-dark" id="navbarNav">
+                    <ul class="navbar-nav d-flex">
+                        <li class="nav-item active">
+                            <a class="nav-link text-light d-flex justify-content-start align-items-center" href="#"><i class="fas fa-home me-2"></i> Acuueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light d-flex justify-content-start align-items-center" href="#"><i class="fas fa-book-open me-2"></i> Catalogue</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light d-flex justify-content-start align-items-center" href="#"><i class="fas fa-info-circle me-2"></i> A propos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light d-flex justify-content-start align-items-center" href="#"><i class="fas fa-envelope me-2"></i> Contact</a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-light me-2 p-2 d-flex justify-content-center align-items-center rounded-pill btn-md-dark"><i class="fas fa-sign-in-alt me-2"></i> Connexion</a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-primary me-2 p-2 d-flex justify-content-center align-items-center rounded-pill"><i class="fas fa-user-plus me-2"></i> Inscription</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
